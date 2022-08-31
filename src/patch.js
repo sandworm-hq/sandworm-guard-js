29cb0c6b106b898a012948f0d1a8c7c26595011f
Gabi Dobocan <gabi@telepat.io>
2022-09-01 01:43:45 +0300

    feat: capture `bind`s, support `minArgsToTrigger`

--- src/patch.js
+++ src/patch.js
@@ -13,6 +13,59 @@ function create(constructor, ...args) {
   return new Factory();
 }
 
+const buildPatch = (family, method, track = () => {}) =>
+  // eslint-disable-next-line func-names
+  function (...args) {
+    let allowed;
+    const {
+      name: module,
+      stack,
+      directCaller,
+      lastModuleCaller,
+      error,
+    } = getCurrentModuleInfo({
+      allowURLs: true,
+    });
+
+    if (
+      typeof method.minArgsToTrigger === 'number' &&
+      (args?.length || 0) < method.minArgsToTrigger
+    ) {
+      allowed = true;
+    } else {
+      logger.debug(`${module} called ${family.name}.${method.name}`);
+      allowed = isModuleAllowedToExecute({
+        module,
+        family,
+        method,
+        directCaller,
+        lastModuleCaller,
+      });
+      track({
+        module,
+        family: family.name,
+        method: method.name,
+        args,
+        allowed,
+        stack,
+        error,
+      });
+    }
+
+    if (allowed) {
+      if (method.isConstructor) {
+        return create(method.original, ...args);
+      }
+      return method.original.apply(this, args);
+    }
+
+    logger.error(`${module} was blocked from calling ${family.name}.${method.name} with`, args);
+
+    throw new SandwormError(
+      `Sandworm: access denied (${module} called ${family.name}.${method.name})`,
+    );
+  };
+
 export default ({family, track = () => {}}) => {
   if (family.available) {
     family.methods.forEach((method) => {
@@ -20,50 +73,10 @@ export default ({family, track = () => {}}) => {
       method.original = family.originalRoot()[method.name];
       if (method.original) {
         logger.debug(`installing ${family.name}.${method.name}`);
-        // eslint-disable-next-line no-inner-declarations
-        function replacement(...args) {
-          const {
-            name: module,
-            stack,
-            directCaller,
-            lastModuleCaller,
-            error,
-          } = getCurrentModuleInfo({
-            allowURLs: true,
-          });
-          logger.debug(`${module} called ${family.name}.${method.name}`);
-          const allowed = isModuleAllowedToExecute({
-            module,
-            family,
-            method,
-            directCaller,
-            lastModuleCaller,
-          });
-          track({
-            module,
-            family: family.name,
-            method: method.name,
-            args,
-            allowed,
-            stack,
-            error,
-          });
-          if (allowed) {
-            if (method.isConstructor) {
-              return create(method.original, ...args);
-            }
-            return method.original.apply(this, args);
-          }
-
-          logger.error(
-            `${module} was blocked from calling ${family.name}.${method.name} with`,
-            args,
-          );
-
-          throw new SandwormError(
-            `Sandworm: access denied (${module} called ${family.name}.${method.name})`,
-          );
-        }
+        // eslint-disable-next-line no-param-reassign
+        method.originalBind = method.original.bind;
+        // eslint-disable-next-line func-names
+        const replacement = buildPatch(family, method, track);
         // eslint-disable-next-line no-restricted-syntax
         for (const prop in method.original) {
           if (Object.prototype.hasOwnProperty.call(method.original, prop)) {
@@ -71,6 +84,11 @@ export default ({family, track = () => {}}) => {
           }
         }
         replacement.prototype = method.original.prototype;
+        replacement.bind = buildPatch(
+          {name: 'bind'},
+          {name: 'args', original: method.originalBind, minArgsToTrigger: 2},
+          track,
+        );
         // eslint-disable-next-line no-param-reassign
         family.originalRoot()[method.name] = replacement;
       }
